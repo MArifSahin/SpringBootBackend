7f1b9d509800225203e2452c7ddec36be4f17044
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-18 12:14:15 +0300

    Add BookResponse dto

---
+++ src/main/java/com/innova/dto/response/BookResponse.java
@@ -0,0 +1,23 @@
+package com.innova.dto.response;
+
+import java.util.Map;
+import java.util.Set;
+
+
+public class BookResponse {
+    private String bookName;
+    private int editorScore;
+    private int userScore;
+    private String editorReview;
+    private Set<String> userReviews;
+    private Map<String,Integer> modes;
+
+    public BookResponse(String bookName, int editorScore, int userScore, String editorReview, Set<String> userReviews, Map<String, Integer> modes) {
+        this.bookName = bookName;
+        this.editorScore = editorScore;
+        this.userScore = userScore;
+        this.editorReview = editorReview;
+        this.userReviews = userReviews;
+        this.modes = modes;
+    }
+}
